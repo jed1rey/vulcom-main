b0bf6c97c24c4cfe1faea676b2ad1f194192fa76
Paula Abib <paula.tws@hotmail.com>
2025-05-10 09:58:14 -0300

    aula10/05

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import AuthGuard from './AuthGuard'
+
 import Homepage from '../pages/Homepage'
 
 import CarForm from '../pages/car/CarForm'
@@ -14,26 +16,42 @@ import UserForm from '../pages/user/UserForm'
 
 import Login from '../pages/Login'
 
+import BruteForce from '../pages/BruteForce'
+
 export default function AppRoutes() {
   return <Routes>
     <Route path="/" element={ <Homepage /> } />
 
     <Route path="/login" element={ <Login /> } />
 
-    <Route path="/cars" element={ <CarList /> } />
-    <Route path="/cars/new" element={ <CarForm /> } />
-    <Route path="/cars/:id" element={ <CarForm /> } />
+    <Route path="/cars" element={ <AuthGuard> <CarList /> </AuthGuard> } />
+    <Route path="/cars/new" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
+    <Route path="/cars/:id" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
 
     <Route path="/customers" element={ 
-      <CustomerList /> 
+      <AuthGuard> <CustomerList /> </AuthGuard> 
     } />
     
-    <Route path="/customers/new" element={ <CustomerForm />} />
-    <Route path="/customers/:id" element={ <CustomerForm />  } />
+    <Route path="/customers/new" element={ 
+      <AuthGuard> <CustomerForm /> </AuthGuard>
+    } />
+    <Route path="/customers/:id" element={ 
+      <AuthGuard> <CustomerForm /> </AuthGuard>  
+    } />
 
-    <Route path="/users" element={ <UserList /> } />
-    <Route path="/users/new" element={ <UserForm /> } />
-    <Route path="/users/:id" element={ <UserForm /> } />
+    <Route path="/users" element={ 
+      <AuthGuard adminOnly={true}> <UserList /> </AuthGuard> } 
+    />
+    <Route path="/users/new" element={ 
+      <AuthGuard adminOnly={true}> <UserForm /> </AuthGuard> } 
+    />
+    <Route path="/users/:id" element={ 
+      <AuthGuard adminOnly={true}> <UserForm /> </AuthGuard> } 
+    />
+
+    <Route path="/brute-force" element={ 
+      <AuthGuard adminOnly={true}> <BruteForce /> </AuthGuard> } 
+    />
     
   </Routes>
 }
